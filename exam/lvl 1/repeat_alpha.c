b48f13b0b9d6ddbca4bd6e4dbed1b1914707c9e1
Alexis Armitano <aarmitan@made-f0Ar6s2.clusters.42paris.fr>
2024-04-04 17:41:20 +0200

    jpp

--- exam/lvl 1/repeat_alpha.c
+++ exam/lvl 1/repeat_alpha.c
@@ -6,7 +6,7 @@
 /*   By: aarmitan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 12:51:31 by aarmitan          #+#    #+#             */
-/*   Updated: 2024/03/30 14:45:20 by aarmitan         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:25:07 by aarmitan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,37 @@
 
 int	main(int argc, char **argv)
 {
-	int	repeat;
-	int	i;
+	int	repeat = 0;
+	int	i = 0;
 
-	if(argc != 2)
-		write(1, "\n", 1);
-	i = 0;
-	repeat = 1;
-	while (argv[1][i] != '\0')
-	{
-		if ((argv[1][i] >= 'a') && (argv[1][i] <= 'z'))
+	if(argc == 2)
+	{	
+		while (argv[1][i] != '\0')
 		{
-			repeat += argv[1][i] - 'a';
+			repeat = 0;
+			if ((argv[1][i] >= 'a') && (argv[1][i] <= 'z'))
+			{
+				repeat += argv[1][i] - 'a' + 1;
+				while (repeat)
+				{
+					write(1, &argv[1][i], 1);
+					repeat--;
+				}
+			}
+			else if ((argv [1][i] >= 'A') && (argv[1][i]))
+			{
+				repeat += argv[1][i] - 'A' + 1;
+				while (repeat)
+				{
+					write(1, &argv[1][i], 1);
+					repeat--;
+				}
+
+			}
+			else
+				write(1, &argv[1][i], 1);
+			i++;
 		}
-		else if ((argv [1][i] >= 'A') && (argv[1][i]))
-		{
-			repeat += argv[1][i] - 'A';
-		}
-		while(repeat)
-		{
-			write(1, &argv[1][i], 1);
-			repeat--;
-		}
-		i++;
 	}
 }
+
