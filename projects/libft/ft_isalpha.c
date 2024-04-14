b710d7ec81d7c1ea477eb710e4650208909c3227
aarmitan <aarmitan@ubuntu.myguest.virtualbox.org>
2024-04-14 10:41:43 +0200

    libft début

---
+++ projects/libft/ft_isalpha.c
@@ -0,0 +1,18 @@
+HEADER
+
+
+int	ft_isalpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (((str[i] >= 'a') && (str[i] <= 'z')) || (str[i] >= 'A') && (str[i] <= 'Z'))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
